08655724f95f95529cc970dea30c524600babe50
Brandon Bosso <brbosso@mines.edu>
2014-11-24 16:55:10 -0700

    Added random preferences for test purposes

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -18,7 +18,13 @@ class UsersTableSeeder extends Seeder {
                 if($count > 1) {
                     $parts = explode(",", $line);
                     if(count($parts) == 4) {
-                        DB::table('users')->insert(array('first'=>trim($parts[0]), 'last'=>trim($parts[1]), 'password'=>Hash::make(trim($parts[2])), 'email'=>trim($parts[3]), 'is_admin'=>false));
+			$pref1 = rand(1,23);
+			$pref2 = rand(1,23);
+			$pref3 = rand(1,23);
+			$pref4 = rand(1,23);
+			$team = rand(0,1);
+                        DB::table('users')->insert(array('first'=>trim($parts[0]), 'last'=>trim($parts[1]), 'password'=>Hash::make(trim($parts[2])), 'email'=>trim($parts[3]), 'is_admin'=>false,
+							 'preference1'=>$pref1, 'preference2'=>$pref2, 'preference3'=>$pref3, 'preference4'=>$pref4, 'teamFirst'=>$team));
                     }
                 }
                 $count++;
